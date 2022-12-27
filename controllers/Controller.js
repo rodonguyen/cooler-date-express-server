900ea71164ca9f9ee69550186d6c1e0f3da4400c
Rodo <rodonguyendd@gmail.com>
2022-12-28 04:22:40 +1000

    Reset

--- controllers/Controller.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  home: (req, res) => {
-    res.render('home');
-  },
-  second: (req, res) => {
-    res.render('second');
-  },
-};
--- routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const basic = require('./controllers/Controller.js');
-
-router.get('/', basic.home);
-router.get('/second', basic.second);
-
-module.exports = router;
